ab295ba8ce3ded937844cd0ac3034b087908fbcc
rafaljus <jusiak.raf@gmail.com>
2015-03-26 23:16:58 +0100

    pierwszy commit

---
+++ ZeMnaJedz/ZeMnaJedz/Models/PassengerModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZeMnaJedz.Models
+{
+    public class PassengerModel
+    {
+
+    }
+}
---
+++ ZeMnaJedz/ZeMnaJedz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZeMnaJedz.Startup))]
+namespace ZeMnaJedz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
